37e364babc6d29a1ab755844b87c83f82c66143f
Ivan Pohorielov <ivan.pohorielov@gmail.com>
2024-12-09 18:22:23 +0200

    Project init

---
+++ SenseTag/SenseTag/SenseTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenseTagApp.swift
+//  SenseTag
+//
+//  Created by Ivan Pohorielov on 09.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SenseTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SenseTag/SenseTagTests/SenseTagTests.swift
@@ -0,0 +1,17 @@
+//
+//  SenseTagTests.swift
+//  SenseTagTests
+//
+//  Created by Ivan Pohorielov on 09.12.2024.
+//
+
+import Testing
+@testable import SenseTag
+
+struct SenseTagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
