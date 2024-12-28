1b4df8ce073f078607fa9195e9c48c6c70156796
Ivan Pohorielov <ivan.pohorielov@gmail.com>
2024-12-28 15:19:03 +0200

    DefaultTextEditorEnviroment+

---
+++ CoreUI/Sources/CoreUI/Components/Inputs/DefaultTextEditor/Enviroment/DefaultTextEditorEnviroment+.swift
@@ -0,0 +1,15 @@
+//
+//  Enviroment+.swift
+//  CoreUI
+//
+//  Created by Ivan Pohorielov on 20.12.2024.
+//
+
+import SwiftUI
+
+extension EnvironmentValues {
+    
+    @Entry
+    var defaultTextEditorHeight: CGFloat = 280.0
+    
+}
