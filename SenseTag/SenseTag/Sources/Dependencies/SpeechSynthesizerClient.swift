b0149cb424fcc12e4ca2720c3dc8dad594d7d33b
Ivan Pohorielov <ivan.pohorielov@gmail.com>
2025-02-08 23:18:59 +0200

    Speech synthesizer voice optimization

--- SenseTag/SenseTag/Sources/Dependencies/SpeechSynthesizerClient.swift
+++ SenseTag/SenseTag/Sources/Dependencies/SpeechSynthesizerClient.swift
@@ -29,10 +29,12 @@ extension SpeechSynthesizerClient: DependencyKey {
                     ?? Locale(identifier: "en-US")
 
                 let utterance = AVSpeechUtterance(string: text)
-                
+
                 // If text locale and current voice locale is the same use prefered
-                let ovverrideVoice = AVSpeechSynthesisVoice.currentLanguageCode() == locale.language.languageCode?.identifier
-                
+                let ovverrideVoice =
+                    AVSpeechSynthesisVoice.currentLanguageCode()
+                    == locale.language.languageCode?.identifier
+
                 utterance.prefersAssistiveTechnologySettings = ovverrideVoice
                 utterance.voice = bestVoice(for: locale)
 
@@ -55,29 +57,37 @@ extension SpeechSynthesizerClient: DependencyKey {
 }
 
 extension SpeechSynthesizerClient {
-    private static func bestVoice(for locale: Locale) -> AVSpeechSynthesisVoice? {
-        
+    fileprivate static func bestVoice(for locale: Locale)
+        -> AVSpeechSynthesisVoice?
+    {
+
         guard let languageCode = locale.language.languageCode?.identifier else {
             return nil
         }
-        
+
         let availableVoices = AVSpeechSynthesisVoice.speechVoices()
-        
+
         // First, try to get a premium quality voice for the specified locale
-        if let premiumVoice = availableVoices.first(where: { $0.language.contains(languageCode) && $0.quality == .premium }) {
+        if let premiumVoice = availableVoices.first(where: {
+            $0.language.contains(languageCode) && $0.quality == .premium
+        }) {
             return premiumVoice
         }
-        
+
         // If no premium Voice, try to get a enhanced voice
-        if let enhancedVoice = availableVoices.first(where: { $0.language.contains(languageCode) && $0.quality == .enhanced }) {
+        if let enhancedVoice = availableVoices.first(where: {
+            $0.language.contains(languageCode) && $0.quality == .enhanced
+        }) {
             return enhancedVoice
         }
-        
+
         // If no enhanced voice, try to get a regular voice
-        if let regularVoice = availableVoices.first(where: { $0.language.contains(languageCode) }) {
+        if let regularVoice = availableVoices.first(where: {
+            $0.language.contains(languageCode)
+        }) {
             return regularVoice
         }
-        
+
         // Default to a fallback voice if no match found
         return AVSpeechSynthesisVoice(identifier: locale.identifier)
     }
