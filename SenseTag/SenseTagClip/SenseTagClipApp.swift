5d1a10dcd673ed1946d4f6592f34cccb6a280452
Ivan Pohorielov <ivan.pohorielov@gmail.com>
2025-03-14 16:34:53 +0200

    AppClip project init

---
+++ SenseTag/SenseTagClip/SenseTagClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenseTagClipApp.swift
+//  SenseTagClip
+//
+//  Created by Ivan Pohorielov on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SenseTagClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SenseTag/SenseTagClipTests/SenseTagClipTests.swift
@@ -0,0 +1,17 @@
+//
+//  SenseTagClipTests.swift
+//  SenseTagClipTests
+//
+//  Created by Ivan Pohorielov on 14.03.2025.
+//
+
+import Testing
+@testable import SenseTagClip
+
+struct SenseTagClipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
